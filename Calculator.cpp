e9b5e066e827f9623ba216229996bc4c3a0a999a
KashyapBhalodiya <kashyapcoding123@gmail.com>
2023-07-14 09:40:31 +0530

    update calculator

--- Calculator.cpp
+++ Calculator.cpp
@@ -18,35 +18,42 @@ int main() {
     }
     cout << "Enter an arithmetic operation (+, -, *, /): ";
     cin >> operation;
-    cout << "Enter the second number: ";
-    cin >> num2;
-    
-    
-    float result;
-    // arithmetic choice
-    switch (operation) {
-        case '+':
-            result = num1 + num2;
-            break;
-        case '-':
-            result = num1 - num2;
-            break;
-        case '*':
-            result = num1 * num2;
-            break;
-        case '/':
-            if (num2 != 0)
-                result = num1 / num2;
-            else {
-                cout << "Error: Division by zero is not allowed." << endl;
-                return 0;
-            }
-            break;
-        default:
-            cout << "Error the values it is only numbers" << endl;
+    if (operation == '+' || operation == '-' || operation == '*' || operation == '/') {
+        cout << "Enter the second number: ";
+        cin >> num2;
+        if (cin.fail()) {
+            cout << "Error: Invalid input. Please write valid input data." << endl;
             return 0;
+        }
+        
+        float result;
+        // arithmetic choice
+        switch (operation) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0)
+                    result = num1 / num2;
+                else {
+                    cout << "Error: Division by zero is not allowed." << endl;
+                    return 0;
+                }
+                break;
+            default:
+                cout << "Error the values it is only numbers" << endl;
+                return 0;
+        }
+        cout << "The result is: " << result << endl;
+    } else {
+        cout << "Error: Invalid input. Please write valid input data." << endl;
     }
-    cout << "The result is: " << result << endl;
     
     return 0;
 }
